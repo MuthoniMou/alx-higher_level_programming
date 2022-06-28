7f9620cdb4561eb62ee4e734fcf787d9531c8a3e
MuthoniMou <maureenmuthoni110@gmail.com>
2022-06-28 08:40:25 -0700

    everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Defining a class """
+
+
+class LockedClass:
+    """
+    This class prevents user from dynamic attributes
+    """
+    __slots__ = ['first_name']
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priv
