d07dccbfac65e33a243b9b7b486f76c9e5b926d9
MuthoniMou <maureenmuthoni110@gmail.com>
2022-07-05 23:03:57 +0300

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+able File  16 lines (13 sloc)  320 Bytes
+
+#!/usr/bin/python3
+"""Module 0-read_file.
+Reads from a file and prints.
+"""
+
+
+def read_file(filename=""):
+    """Reads from filename and prints
+    its contents to stdout.
+    Args:
+        - filename: name of the file
+    """
+
+    with open(filename) as f:
+        read_text = f.read()
+        print(read_text, end="")
